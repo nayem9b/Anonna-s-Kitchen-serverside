9a81e7e7e19b77055210ba3611641d7d87f1570a
nayem9b <razwanniam1@gmail.com>
2022-11-07 21:01:19 +0600

    all npm packages installed and project initialized

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { MongoClient, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dafmrk2.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  process.env.JWT_SECRET,
+  uri
+);
+const client = new MongoClient(uri);
+const Product = client.db("practiceRun").collection("products");
+
+app.listen(port, () => {
+  console.log(`port is running on ${port}`);
+});
